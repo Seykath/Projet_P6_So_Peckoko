07d7f8a9ad2ced73c191f83d5abdf6c76c206fd0
Tony <tony.roussarie@gmail.com>
2020-08-21 16:35:21 +0200

    ajouter server.js et app.js

---
+++ backend/app.js
@@ -0,0 +1,5 @@
+const express = require('express'); // Important de express
+const app = express(); // Création d'une application express
+
+
+module.exports = app; // Exportation de la const app pour y accèder depuis les autres fichiers.
